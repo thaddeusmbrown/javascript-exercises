842f7f1bdab81ba48be69ddb789c845fdf5b4513
thaddeusmbrown <thaddeusmbrown@gmail.com>
2021-12-22 16:00:15 -0600

    completed 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const sum = function(array) {
+  const total = array.reduce((sum, num) => {
+    return sum + num;
+  }, 0);
+  return total;
+};
+
+const multiply = function(array) {
+  const total = array.reduce((product, num) => {
+    return product*num;
+  }, 1);
+  return total;
+
+};
+
+const power = function(num1, num2) {
+  return num1 ** num2;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factored = 1;
+  if (num == 0) {
+    return 1;
+  } else {
+    for (let i = 1; i<=num; i++) {
+      factored = factored * i;
+    }
+  }
+  return factored;
 };
 
 // Do not edit below this line
