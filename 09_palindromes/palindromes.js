7a42e88b867331ed9064f255a4c55fb241f0396b
thaddeusmbrown <thaddeusmbrown@gmail.com>
2021-12-22 16:19:56 -0600

    completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const stripped = string
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        .split(' ').join('').toLowerCase();
+    const split = stripped.split('');
+    const reversed = split.reverse();
+    const joined = reversed.join('');
+    return stripped == joined;
 
 };
 
