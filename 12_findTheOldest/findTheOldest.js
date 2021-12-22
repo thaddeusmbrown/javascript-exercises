51271b437de4fc27bded4280bdab6fd2fd8a4519
thaddeusmbrown <thaddeusmbrown@gmail.com>
2021-12-22 16:41:19 -0600

    completed exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(names) {
+    const oldest = names.reduce((lastPerson, nextPerson) => {
+        if (!lastPerson.yearOfDeath) {
+            lastPerson.yearOfDeath = new Date().getFullYear();
+        }
+        if (!nextPerson.yearOfDeath) {
+            nextPerson.yearOfDeath = new Date().getFullYear();
+        }
+        const lastAge = lastPerson.yearOfDeath - lastPerson.yearOfBirth;
+        const nextAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        return (lastAge > nextAge) ? lastPerson : nextPerson;
+    });
+    return oldest;
 };
 
 // Do not edit below this line
