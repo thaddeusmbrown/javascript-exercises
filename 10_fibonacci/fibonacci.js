23758352cec56ff295816d1f4a5d89861525a017
thaddeusmbrown <thaddeusmbrown@gmail.com>
2021-12-22 16:25:44 -0600

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    let lastNum = 0;
+    let fib = 1;
+    for (let i = 2; i <= num; i++) {
+        fib += lastNum;
+        lastNum = fib - lastNum;
+    }
+    return fib;
 };
 
 // Do not edit below this line
